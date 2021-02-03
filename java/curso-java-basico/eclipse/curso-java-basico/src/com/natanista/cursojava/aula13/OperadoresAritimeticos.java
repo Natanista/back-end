0818b53b1d0690c95f5ad70627e502e60612e2d5
natanista <gdaynatan@gmail.com>
2021-02-03 20:36:42 -0300

    aula 13

---
+++ java/curso-java-basico/eclipse/curso-java-basico/src/com/natanista/cursojava/aula13/OperadoresAritimeticos.java
@@ -0,0 +1,42 @@
+package com.natanista.cursojava.aula13;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String nome1 = "Esse é ";
+		String nome2 = "meu nome";
+		System.out.println(nome1 + nome2);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+		++resultado;
+		System.out.println(resultado);
+		
+		--resultado;
+		System.out.println(resultado);
+		
+		resultado--;
+		System.out.println(resultado);
+	}
+
+}
---
+++ java/curso-java-basico/eclipse/curso-java-basico/src/com/natanista/cursojava/aula13/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+package com.natanista.cursojava.aula13;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 1;
+		int valor2 = 2;
+		
+		System.out.println(valor1 == 1 && valor2 == 2);
+
+	}
+
+}
