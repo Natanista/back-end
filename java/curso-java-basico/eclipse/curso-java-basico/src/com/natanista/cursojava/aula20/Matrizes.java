bf5f31feb0ef77f441a443b4cd4f046995acbefc
natanista <gdaynatan@gmail.com>
2021-03-09 00:28:24 -0300

    exercicios arrays multidimensionais

--- java/curso-java-basico/eclipse/curso-java-basico/src/com/natanista/cursojava/aula20/Matrizes.java
+++ java/curso-java-basico/eclipse/curso-java-basico/src/com/natanista/cursojava/aula20/Matrizes.java
@@ -4,7 +4,7 @@ public class Matrizes {
 
     public static void main(String[] args) {
 
-        Double[][] notasAlunos = new Double[30][4];
+        Double[][] notasAlunos = new Double[3][4];
 
         notasAlunos[0][0] = 10.0;
         notasAlunos[0][1] = 8.0;
@@ -15,10 +15,20 @@ public class Matrizes {
         notasAlunos[1][1] = 8.0;
         notasAlunos[1][2] = 10.0;
         notasAlunos[1][3] = 9.0;
-        
+
         notasAlunos[2][0] = 10.0;
         notasAlunos[2][1] = 8.0;
         notasAlunos[2][2] = 10.0;
         notasAlunos[2][3] = 9.0;
+
+        notasAlunos[1][3] = 8.0;
+        Double soma = 0.0;
+        for(int i = 0; i < notasAlunos.length;i++) {
+           soma = 0.0;
+            for (int j = 0; j <= notasAlunos.length; j++) {
+                soma += notasAlunos[i][j];
+            }
+            System.out.println("Media do aluno: " + (soma/4));
+        }
     }
 }
