fa550edf45191091e32f2574164e5702ba9771ec
natanista <gdaynatan@gmail.com>
2021-03-08 23:56:29 -0300

    exercicios de java -  matrizes

---
+++ java/curso-java-basico/eclipse/curso-java-basico/src/com/natanista/cursojava/aula20/Matrizes.java
@@ -0,0 +1,24 @@
+package com.natanista.cursojava.aula20;
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        Double[][] notasAlunos = new Double[30][4];
+
+        notasAlunos[0][0] = 10.0;
+        notasAlunos[0][1] = 8.0;
+        notasAlunos[0][2] = 10.0;
+        notasAlunos[0][3] = 9.0;
+
+        notasAlunos[1][0] = 10.0;
+        notasAlunos[1][1] = 8.0;
+        notasAlunos[1][2] = 10.0;
+        notasAlunos[1][3] = 9.0;
+        
+        notasAlunos[2][0] = 10.0;
+        notasAlunos[2][1] = 8.0;
+        notasAlunos[2][2] = 10.0;
+        notasAlunos[2][3] = 9.0;
+    }
+}
